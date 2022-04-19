766782a14f78be2f10e8a42fbd6033b8a670337a
rufus drew <rufusdrew3@gmail.com>
2022-04-19 15:09:03 +0530

    commit on main

--- src/App.vue
+++ src/App.vue
@@ -61,6 +61,7 @@ export default {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+   background:url("http://www.miracolieucaristici.org/miracoli_images/splash_bkg_01.jpg");
 }
 .container {
   max-width: 1440px;
