c5ebde07391daef348e1f984ed2914116b91a8f5
rufus drew <rufusdrew3@gmail.com>
2022-04-23 09:58:20 +0530

    footer_edit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -38,7 +38,7 @@
         </div>
       </div>
       <div class="right">
-        <p>Copyright By Rosarians!! {{ currentYear }}</p>
+        <p>Copyright By Rosarians {{ currentYear }}</p>
         <p>Created By Rufus Drew</p>
       </div>
     </div>
